86350d83260c45bf43752a572952ae9b780bf281
Youssef <yousefkamel282@gmail.com>
2026-09-16 11:46:14 +0300

    created some enums

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATED_SUCCESS = "file_validate_successfully"
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDED = "file_size_exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
+    FILE_PROCESSING_SUCCESS = "file_processing_success"
+    FILE_PROCESSING_FAILED = "file_processing_failed"
+    FILE_ID_ERROR = "file_id_error"
+    NO_FILES_ERROR = "no_files_error"
+    PROJECT_NOT_FOUND_ERROR = "project_not_found"
+    INSERT_INTO_VECTORDB_ERROR = "insert_into_vectordb_error"
+    INSERT_INTO_VECTORDB_SUCCESS = "insert_into_vectordb_success"
+    VECTORDB_COLLECTION_RETRIEVED = "vectordb_collection_retrieved"
+    VECTORDB_SEARCH_ERROR = "vectordb_search_error"
+    VECTORDB_SEARCH_SUCCESS = "vectordb_search_success"
+    RAG_ANSWER_ERROR = "rag_answer_error"
+    RAG_ANSWER_SUCCESS = "rag_answer_success"
+    
