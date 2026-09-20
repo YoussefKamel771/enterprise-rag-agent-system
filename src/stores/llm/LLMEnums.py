ac3362d26061f81725bad5ea64a57ce0804eca8d
Youssef <yousefkamel282@gmail.com>
2026-09-20 19:58:24 +0300

    added llm factory

---
+++ src/stores/llm/LLMEnums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class LLMEnums(Enum):
+    OPENAI = "OPENAI"
+    COHERE = "COHERE"
+    SENTENCE_TRANSFORMER = "SENTENCE_TRANSFORMER"
+    
+    
+class OpenAIEnums(Enum):
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
+class COHEREEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"  
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+class DocumentTypeEnums(str, Enum):
+    QUERY = "query"
+    DOCUMENT = "document"
